703235d164a3961bbd7bebfcffd5d84c0c9b6308
Remy Prince <remyprince97@gmail.com>
2023-05-23 15:50:32 +0200

    done

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  get 'tasks/new', to: 'tasks#new', as: :new
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  patch 'tasks/:id', to: 'tasks#update'
+  post 'tasks', to: 'tasks#create'
+  delete 'tasks/:id', to: 'tasks#destroy', as: :destroy
 end
